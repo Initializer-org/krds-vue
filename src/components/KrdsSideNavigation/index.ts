cdad7923fcb44329ac00022841f8224413996e91
hn.lee <y900529@gmail.com>
2025-09-10 22:06:08 +0900

    feat: KrdsSideNavigation 컴포넌트 및 스토리북 추가

---
+++ src/components/KrdsSideNavigation/index.ts
@@ -0,0 +1,16 @@
+/**
+ * KRDS SideNavigation 컴포넌트
+ */
+
+import KrdsSideNavigation from './KrdsSideNavigation.vue'
+import type {
+  KrdsSideNavigationProps,
+  KrdsSideNavigationEmits,
+  SideNavItem,
+  SideNavSubItem,
+  SideNavPopupItem
+} from './KrdsSideNavigation.vue'
+
+export { KrdsSideNavigation }
+export type { KrdsSideNavigationProps, KrdsSideNavigationEmits, SideNavItem, SideNavSubItem, SideNavPopupItem }
+export default KrdsSideNavigation
