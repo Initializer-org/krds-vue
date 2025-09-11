dbf8b39297b2c5973b40e1b33c2b5b559c6d2466
hn.lee <y900529@gmail.com>
2025-09-11 19:39:34 +0900

    feat: KrdsCriticalAlerts 추가

---
+++ src/components/KrdsCriticalAlerts/index.ts
@@ -0,0 +1,10 @@
+/**
+ * KRDS Critical Alerts 컴포넌트
+ */
+
+import KrdsCriticalAlerts from './KrdsCriticalAlerts'
+import type { KrdsCriticalAlertsProps, KrdsCriticalAlertsEmits, AlertType } from './KrdsCriticalAlerts'
+
+export { KrdsCriticalAlerts }
+export type { KrdsCriticalAlertsProps, KrdsCriticalAlertsEmits, AlertType }
+export default KrdsCriticalAlerts
