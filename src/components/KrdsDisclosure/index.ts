dd93f3c009d428fee1edd8c80637bec991aee241
hn.lee <y900529@gmail.com>
2025-09-11 20:04:11 +0900

    feat: KrdsDisclosure 컴포넌트 추가

---
+++ src/components/KrdsDisclosure/index.ts
@@ -0,0 +1,10 @@
+/**
+ * KRDS Disclosure 컴포넌트
+ */
+
+import KrdsDisclosure from './KrdsDisclosure'
+import type { KrdsDisclosureProps, KrdsDisclosureEmits } from './KrdsDisclosure'
+
+export { KrdsDisclosure }
+export type { KrdsDisclosureProps, KrdsDisclosureEmits }
+export default KrdsDisclosure
