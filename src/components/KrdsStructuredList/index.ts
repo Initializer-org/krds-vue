d048f5e50c8e5142c8b18e0225c584dae91e315e
hn.lee <y900529@gmail.com>
2025-09-11 20:54:04 +0900

    feat: KrdsStructuredList 컴포넌트 추가

---
+++ src/components/KrdsStructuredList/index.ts
@@ -0,0 +1,10 @@
+/**
+ * KRDS StructuredList 컴포넌트
+ */
+
+import KrdsStructuredList from './KrdsStructuredList'
+import type { KrdsStructuredListProps } from './KrdsStructuredList'
+
+export { KrdsStructuredList }
+export type { KrdsStructuredListProps }
+export default KrdsStructuredList
