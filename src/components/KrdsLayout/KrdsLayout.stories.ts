ba6941cfff63935b4166c698d296662dd43324ee
nowrica <hn.lee@initializer.org>
2025-09-17 10:55:13 +0900

    chore: add KrdsFooter and KrdsIdentifier to KrdsLayout story

--- src/components/KrdsLayout/KrdsLayout.stories.ts
+++ src/components/KrdsLayout/KrdsLayout.stories.ts
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import KrdsLayout from './KrdsLayout'
 import { KrdsMasthead } from '../KrdsMasthead'
+import { KrdsFooter } from '../KrdsFooter'
+import { KrdsIdentifier } from '../KrdsIdentifier'
 
 const meta: Meta<typeof KrdsLayout> = {
   title: 'Components/ETC/KrdsLayout',
@@ -36,7 +38,7 @@ type Story = StoryObj<typeof meta>
  */
 export const Default: Story = {
   render: () => ({
-    components: { KrdsLayout, KrdsMasthead },
+    components: { KrdsLayout, KrdsMasthead, KrdsFooter, KrdsIdentifier },
     template: `
       <KrdsLayout>
         <KrdsSkipLink href="#container">본문 바로가기</KrdsSkipLink>
@@ -48,6 +50,70 @@ export const Default: Story = {
             </div>
           </div> 
         </div>
+        <KrdsFooter v-bind="args">
+          <template #top>
+            <button type="button" class="link" title="관련 사이트 레이어">related_site</button>
+            <button type="button" class="link" title="관련 사이트 레이어">related_site</button>
+            <button type="button" class="link" title="관련 사이트 레이어">related_site</button>
+            <button type="button" class="link" title="관련 사이트 레이어">related_site</button>
+          </template>
+
+          <template #logo>
+            <span class="sr-only">KRDS - Korea Design System</span>
+          </template>
+
+          <template #content>
+            <div class="f-info">
+              <p class="info-addr">(26464) 강원특별자치도 원주시 건강로 32(반곡동) 국민건강보험공단</p>
+              <ul class="info-cs">
+                <li><strong class="strong">대표전화 1577-1000</strong><span class="span">(유료, 평일 09시~18시)</span></li>
+                <li><strong class="strong">해외이용 82-33-811-2001</strong><span class="span">(유료, 평일 09시~18시)</span></li>
+              </ul>
+            </div>
+
+            <div class="f-link">
+              <div class="link-go">
+                <KrdsButton text size="medium">찾아오시는 길 <i class="svg-icon ico-angle right"></i></KrdsButton>
+                <KrdsButton text size="medium">이용안내 <i class="svg-icon ico-angle right"></i></KrdsButton>
+                <KrdsButton text size="medium">직원검색 <i class="svg-icon ico-angle right"></i></KrdsButton>
+              </div>
+              <div class="link-sns">
+                <a href="#" class="krds-btn xlarge icon border" target="_blank" title="새 창 열기">
+                  <span class="sr-only">인스타그램</span>
+                  <i class="svg-icon ico-instagram"></i>
+                </a>
+                <a href="#" class="krds-btn xlarge icon border" target="_blank" title="새 창 열기">
+                  <span class="sr-only">유튜브</span>
+                  <i class="svg-icon ico-youtube"></i>
+                </a>
+                <a href="#" class="krds-btn xlarge icon border" target="_blank" title="새 창 열기">
+                  <span class="sr-only">X</span>
+                  <i class="svg-icon ico-sns-x"></i>
+                </a>
+                <a href="#" class="krds-btn xlarge icon border" target="_blank" title="새 창 열기">
+                  <span class="sr-only">페이스북</span>
+                  <i class="svg-icon ico-facebook"></i>
+                </a>
+                <a href="#" class="krds-btn xlarge icon border" target="_blank" title="새 창 열기">
+                  <span class="sr-only">블로그</span>
+                  <i class="svg-icon ico-blog"></i>
+                </a>
+              </div>
+            </div>
+          </template>
+
+          <template #bottom>
+            <div class="f-btm-text">
+              <div class="f-menu">
+                <a href="#" class="point">개인정보처리방침</a>
+                <a href="#">저작권 정책</a>
+                <a href="#">웹 접근성 품질인증 마크 획득</a>
+              </div>
+              <p class="f-copy">© 2023 National Health Insurance Service. All rights reserved.</p>
+            </div>
+            <KrdsIdentifier>이 누리집은 보건복지부 누리집입니다.</KrdsIdentifier>
+          </template>
+        </KrdsFooter>
       </KrdsLayout>
     `
   })
