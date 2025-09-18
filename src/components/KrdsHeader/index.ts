e7314817f6075a2219bbd66df760b769ae6ee0c8
nowrica <hn.lee@initializer.org>
2025-09-18 16:40:15 +0900

    chore: add KrdsHeader component and integrate it into KrdsLayout story

---
+++ src/components/KrdsHeader/index.ts
@@ -0,0 +1,10 @@
+/**
+ * KRDS Header 컴포넌트
+ */
+
+import KrdsHeader from './KrdsHeader'
+import type { KrdsHeaderProps } from './KrdsHeader'
+
+export { KrdsHeader }
+export type { KrdsHeaderProps }
+export default KrdsHeader
